86440862e0a750c5885e3cb3bd41c79384aeb294
Mehrin-Anannya <anannya.mehrin@gmail.com>
2023-01-31 00:11:23 +0600

    Add comments for poi use for Excel read & write

--- Customer Data View Project/src/RunProject.java
+++ Customer Data View Project/src/RunProject.java
@@ -4,6 +4,10 @@ public class RunProject {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//download poi 5.2.3 from server 
+		//Then add jars from Build Path> Select Classpath> Add External Libraries 
+		//> Locate poi jar files location extracting from inner folders
+		// TODO Auto-generated method stub
 		CustomerDataExecute customerDataExecute = new CustomerDataExecute();
 		customerDataExecute.writeData(null);
 		//customerDataExecute.viewData(null);
